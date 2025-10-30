064074c556b88abde08ec741a6d7cf236db1de17
ttchef <ruro0.edit@gmail.com>
2025-10-30 15:26:44 +0100

    adds basic manager structure of editor

---
+++ editor/include/Manager.h
@@ -0,0 +1,14 @@
+
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <stdint.h>
+
+typedef struct Manager {
+    int32_t windowWidth;
+    int32_t windowHeight;
+    int32_t guiWidth;
+    int32_t guiHeight;
+} Manager;
+
+#endif
--- editor/src/main.c
+++ editor/src/main.c
@@ -1,15 +1,53 @@
 
 #include <raylib.h>
+#include <stdint.h>
+
+#include "Manager.h"
+
+void update(Manager* manager);
+void draw(Manager* manager);
+void drawGui(Manager* manager);
 
 int main() {
 
-    InitWindow(800, 600, "Editor");
+    Manager manager = {
+        .windowWidth = 1280,
+        .windowHeight = 780,
+        .guiWidth = 300,
+        .guiHeight = 780,
+    };
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(manager.windowWidth, manager.windowHeight, "Editor");
 
     while (!WindowShouldClose()) {
-       
+        update(&manager);
+
+        BeginDrawing();
+            ClearBackground(BLACK);
+            draw(&manager);
+            drawGui(&manager);
+        EndDrawing();
     }
 
     CloseWindow();
 
     return 0;
 }
+
+void update(Manager* manager) {
+    int32_t a = manager->windowWidth;
+    a++;
+}
+
+void draw(Manager* manager) {
+    int32_t a = manager->windowWidth;
+    a++;   
+}
+
+void drawGui(Manager* manager) {
+    int32_t guiStart = manager->windowWidth - manager->guiWidth;
+
+    DrawRectangle(guiStart, 0, manager->guiWidth, manager->guiHeight, DARKGRAY);
+}
+
